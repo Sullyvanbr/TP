50b27fbaa99d03894536a075f5cc1a419379851c
Sullyvanbr <sullyvanbrillouetformation@gmail.com>
2026-05-11 16:31:30 +0200

    vault backup: 2026-05-11 16:31:30

--- JS Ex/ex1.js
+++ JS Ex/ex1.js
@@ -7,3 +7,28 @@ let p = maFonction;
 function maFonction() {
     console.log("Hello World");
 }
+
+let nombres = uneFonction()
+function uneFonction() {
+    return 0.1 + 0.2
+}
+
+let unCompteur = 0;
+let b = ++unCompteur;
+let c = --unCompteur;
+let d = unCompteur +=10;
+let e = unCompteur + 0.1;
+let puissance = 2**4;
+
+console.log(b);
+console.log(c);
+console.log(unCompteur);
+console.log(e);
+console.log(puissance);
+console.log(3**4 + 5 % 3 / (3 * 8) - 4);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+console.log(getRandomInt(10));
