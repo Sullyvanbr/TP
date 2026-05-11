06212824d7089b6d365cb9d5226deb1ca2fb794a
Sullyvanbr <sullyvanbrillouetformation@gmail.com>
2026-05-11 14:20:22 +0200

    vault backup: 2026-05-11 14:20:22

---
+++ JS Ex/ex1.js
@@ -0,0 +1,9 @@
+let cars = ["Saab", "BMW", "Toyota"];
+let chaine = "chaine de caractères";
+let nombre = "1";
+let nombreVirgule = "1,1";
+const ages = {bob: 27, pierre: 30};
+let p = maFonction;
+function maFonction() {
+    console.log("Hello World");
+}
