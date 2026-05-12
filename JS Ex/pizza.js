12592632777e58ea67d5fdb38a177c27a74b6649
Sullyvanbr <sullyvanbrillouetformation@gmail.com>
2026-05-12 11:06:25 +0200

    vault backup: 2026-05-12 11:06:25

---
+++ JS Ex/pizza.js
@@ -0,0 +1,13 @@
+let prenom = "Tot";
+let addresse = "11 avenue de l'europe"
+let ville = "Paris";
+let numéro_de_telephone = "XX.XX.XX.XX.XX"
+let commande = "Regina"
+let temps = "30 minutes"
+let enseigne = "\"La Pizzeria Raffinata\""
+
+let SumUpOrderPhrase = `Bonjour Mr ${prenom}, votre pizza :${commande} est en cours de préparation. Elle vous sera livrée d'ici ${temps} au ${addresse}, ${ville}. À l'arrivé, le livreur vous appellera au: ${numéro_de_telephone}.
+Merci d'avoir commandé chez ${enseigne}
+Ps: Qu'est-ce qu'une pizza dit à une autre pizza qui lui fait de l'ombre ? 
+\"Arrête de me trancher la route !\"`;
+console.log(SumUpOrderPhrase)
